d5b155839e9716d91a97a70da1a8a8f611f3ae58
Nicolas <nicolas.bouffanais.1999@gmail.com>
2024-06-05 23:46:43 -0400

    background color,navbar, footer tailwindcss install

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,20 +4,63 @@
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-    </div>
+    <nav class="flex h-20 bg-[#141627]">
+      <ul class="flex w-[90dvw] mx-auto justify-between items-center">
+        <li class="flex items-center text-4xl font-bold text-white">
+          Undock Awaited
+        </li>
+        <li>
+          <button class="bg-red-400"></button>
+        </li>
+      </ul>
+    </nav>
   </header>
 
   <main>
-    <h1>Hola</h1>
-    <div>
-      Aqui estarian los graficos 
+    <div class="grid">
+
     </div>
   </main>
+  <footer>
+    <p class="text-white font-bold text-lg">Designed & Built by Nicolas Bouffanais</p>
+  </footer>
 </template>
 
-<style scoped>
+<style>
+html {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-size: 10px;
+}
 
+main {
+  width: 100dvw;
+  background-color: #141627;
+  height: 100dvh;
+}
+footer{
+  background-color: #131416;
+  width: 100dvw;
+  height: 12rem;
+  padding: 3rem 7vw;
+  margin: 0 auto;
+}
+::-webkit-scrollbar {
+  width: 1rem;
+  height: 0.3rem;
+}
+
+::-webkit-scrollbar-button {
+  background-color: #666;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #666;
+  border-radius: 3px;
+}
+
+::-webkit-scrollbar-track-piece {
+  background-color: #000;
+}
 </style>
