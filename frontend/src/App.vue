4ec11b8b3f513ecd9b35143fe714a362b5931109
Nicolas <nicolas.bouffanais.1999@gmail.com>
2024-06-09 19:17:31 -0400

    css update, component dashboard ts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
+import Dashboard from './components/Dashboard.vue';
 
 </script>
 
 <template>
   <header>
-    <nav class="flex h-20 bg-[#141627]">
+    <nav class="flex h-24 bg-[#141b2d]">
       <ul class="flex w-[90dvw] mx-auto justify-between items-center">
         <li class="flex items-center text-4xl font-bold text-white">
           Undock Awaited
         </li>
         <li>
-          <button class="bg-red-400"></button>
+          <button class="text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512">
+              <path fill="currentColor"
+                d="M264 480A232 232 0 0 1 32 248c0-94 54-178.28 137.61-214.67a16 16 0 0 1 21.06 21.06C181.07 76.43 176 104.66 176 136c0 110.28 89.72 200 200 200c31.34 0 59.57-5.07 81.61-14.67a16 16 0 0 1 21.06 21.06C442.28 426 358 480 264 480" />
+            </svg>
+          </button>
         </li>
       </ul>
     </nav>
   </header>
 
-  <main>
-    <div class="grid">
-
-    </div>
+  <main class="flex justify-center py-6">
+    <Dashboard />
   </main>
   <footer>
     <p class="text-white font-bold text-lg">Designed & Built by Nicolas Bouffanais</p>
@@ -27,16 +31,18 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
 html {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-size: 10px;
+  font-family: 'Roboto', 'Helvetica';
 }
 
 main {
   width: 100dvw;
-  background-color: #141627;
+  background-color: #141b2d;
   height: 100dvh;
 }
 footer{
---
+++ frontend/src/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup >
+
+</script>
+<template>
+  <div class="grid gap-2 w-11/12 h-[300px]">
+    <div class="bg-[#222a40] w-[300px] h-[200px] rounded-2xl">
+
+    </div>
+    <div class="bg-[#222a40] w-[300px] h-[200px] rounded-2xl">
+
+    </div>
+  </div>
+</template>
+<style>
+
+</style>
